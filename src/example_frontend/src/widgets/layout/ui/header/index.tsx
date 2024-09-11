30804e10603279384acf8bec7792aa7feb2b4f10
Leontiy <leosungun@gmail.com>
2024-09-11 17:45:15 +0300

    style header

--- src/example_frontend/src/widgets/layout/ui/header/index.tsx
+++ src/example_frontend/src/widgets/layout/ui/header/index.tsx
@@ -1,21 +1,36 @@
-import { Stack } from '@mui/material';
+import { Stack, styled, Typography } from '@mui/material';
 import Logo from '../assets/logo.svg?react';
 import { Link } from 'react-router-dom';
 import { useAuth } from '@ic-reactor/react';
+
+const StyledLink = styled(Link)`
+  color: #fff;
+  text-decoration: none;
+  font-size: 16px;
+  font-weight: 800;
+`;
+
+const RouterLink = ({ to, children }: { to: string, children: React.ReactNode }) => {
+  return (
+    <StyledLink to={to}>
+      <Typography sx={{ color: '#fff' }}>{children}</Typography>
+    </StyledLink>
+  )
+};
 
 export const Header = () => {
   const { authenticated } = useAuth();
 
   return (
-    <Stack direction="row" justifyContent="space-between" alignItems="center" px={2.5} pt={2.5}>
-      <Stack direction="row" alignItems="center" spacing={2}>
+    <Stack direction="row" justifyContent="space-between" alignItems="center" px={2.5} py={2.5} sx={{ background: 'linear-gradient(98.82deg, #D8CCE3 1.85%, #B3DDF8 136.36%)' }}>
+      <Stack direction="row" alignItems="center" width="100%" paddingX={2} justifyContent="space-between" spacing={2}>
         <Logo width="80px" height="80px" />
         {
           authenticated && (
             <Stack direction="row" spacing={2}>
-              <Link to="/">Main</Link>
-              <Link to="/feed">Feed</Link>
-              <Link to="/form">Form</Link>
+              <RouterLink to="/">Main</RouterLink>
+              <RouterLink to="/feed">Feed</RouterLink>
+              <RouterLink to="/form">Form</RouterLink>
             </Stack>
     
           )
